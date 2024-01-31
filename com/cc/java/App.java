f0f34bd1f3a1f9c052decb89389e20ceb0649826
BK <ck.comcave@proton.me>
2024-01-31 10:33:30 +0100

    Polymorphie

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,11 +3,43 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
+
+        Queen queen = new Queen();
+        Worker worker = new Worker();
+        Drone drone = new Drone();
      
+
+        pollObj(queen);
+        pollObj(worker);
+        pollObj(drone);
+
+        output(queen.doYourJob());
+        output(queen.fly());
+        output("------");
+
+        output(worker.doYourJob());
+        output(worker.fly());
+        output("------");
+
+        output(drone.doYourJob());
+        output(drone.fly());
+        output("------");
+
         
+
     }
 
-   
+   private static void pollObj(HoneyBee obj){
+
+    output(obj.doYourJob());
+    output(obj.fly());
+    output("------");
+
+
+
+
+
+   }
    
     private static void output(String outputStr) {
         System.out.println(outputStr);
